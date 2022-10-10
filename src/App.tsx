ce4875a32cdb48aef94572821dcf4b1fe84199ed
osmangokmenx <52015838+osmangokmenx@users.noreply.github.com>
2022-10-10 15:00:56 +0300

    initial commit: basic form design

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,101 @@
-import * as React from "react"
+import { FormEvent, useState } from "react";
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Input,
+  Button,
+  InputGroup,
+  FormControl,
+  InputRightElement,
+  Flex,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+export const App = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowClick = () => setShowPassword(!showPassword);
+  const onSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Fill all fields");
+    } else if (password.length < 6) {
+      setError("password must be at least 6 characters");
+    } else {
+      setError("");
+    }
+  };
+  return (
+    <ChakraProvider theme={theme}>
+      <Flex
+        flexDirection="column"
+        width="100wh"
+        height="100vh"
+        backgroundColor="gray.200"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box textAlign="center" fontSize="xl">
+          <Stack
+            flexDir="column"
+            mb="2"
+            justifyContent="center"
+            alignItems="center"
           >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+            <Box minW={{ base: "90%", md: "468px" }}>
+              <form onSubmit={onSubmitHandler}>
+                <Stack
+                  spacing={4}
+                  p="1rem"
+                  backgroundColor="whiteAlpha.900"
+                  boxShadow="md"
+                >
+                  <FormControl>
+                    <InputGroup>
+                      <Input
+                        type="email"
+                        placeholder="email address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl>
+                    <InputGroup>
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                          {showPassword ? "Hide" : "Show"}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+                  <Button
+                    borderRadius={0}
+                    type="submit"
+                    variant="solid"
+                    colorScheme="teal"
+                    width="full"
+                  >
+                    Login
+                  </Button>
+                  <Text color="tomato">{error}</Text>
+                </Stack>
+              </form>
+            </Box>
+          </Stack>
+        </Box>
+      </Flex>
+    </ChakraProvider>
+  );
+};
